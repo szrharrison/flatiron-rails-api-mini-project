8b209d68184de92ac28e5c6162ddcf47ae39127e
szrharrison <s.z.r.harrison@gmail.com>
2017-05-04 18:50:52 -0400

    Seeded our database
    Built the seed file and added the necessary gems to seed the databse.

--- mini-project-api/db/seeds.rb
+++ mini-project-api/db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'redd'
+require 'metainspector'
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+reddit = Redd.it(
+
+)
+
+links = reddit.search("futurism.com", limit: 100).map do |article|
+  if article.url =~ /\.com/
+    article.url
+  else
+    nil
+  end
+end.compact
+
+links.each do |link|
+  begin
+    page = MetaInspector.new(link)
+
+    html_data = open(link.sub(/http\b/, 'https')).read
+    nokogiri_object = Nokogiri::HTML(html_data)
+    header = nokogiri_object.css('.post-header-full').to_html
+    if !!/background-image: url\((.+?)\)/.match(header)
+      title = page.title
+      content = nokogiri_object.css('.summary.module').to_html
+      img = /background-image: url\((.+?)\)/.match(header)[1]
+      description = nokogiri_object.css('.synopsis.module').to_html
+      tags = page.meta["article:tag"].split(' ')
+      category = page.meta["article:section"]
+      @category = Category.find_or_create_by(name: category)
+      tags.map! do |tag|
+        @tag = Tag.find_or_create_by(name: tag)
+        @tag
+      end
+      puts 'got here'
+      @article = Article.create_with(content: content, image: img, description: description, tags: tags, category: @category).find_or_create_by(title: title)
+    end
+  rescue
+  end
+end
